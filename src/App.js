2354c445ed83c7233eb3d42c69c82b2ae6999c88
Axel Thiel <axth2220@gmail.com>
2020-12-10 15:54:49 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import React from 'react'
+import {Switch, Route, Link} from 'react-router-dom'
+
 function App() {
-  return <div className="Container">Stockmarket</div>;
+  return (
+    <div className="Container">Stockmarket
+
+    <Link to="/crypto">Crypto</Link>
+    <Link to="/currencies">Currencies</Link>
+    <Link to="/indices">Indices</Link>
+    <Link to="/markets">Markets</Link>
+
+    <Switch>
+      <Route path="/crypto">
+        crypto
+      </Route>
+      <Route path="/currencies">
+      currencies
+      </Route>
+
+      <Route path="/indices">
+    indices
+      </Route>
+
+      <Route path="/markets">
+markets
+      </Route>
+
+
+      <Route path="/">
+        
+      </Route>
+    </Switch>
+    </div>
+  )
 }
 
 export default App;
