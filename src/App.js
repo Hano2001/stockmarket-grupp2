a2bebc5bdf8c260259818536a6e3ebaca563eb30
Oz90 <gurr__@hotmail.com>
2020-12-10 18:35:38 +0100

    Add a indices page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
-import React from 'react'
-import {Switch, Route, Link} from 'react-router-dom'
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { IndicesPage } from "./pages/IndicesPage";
 
 function App() {
   return (
-    <div className="Container">Stockmarket
+    <div className="Container">
+      Stockmarket
+      <Link to="/crypto">Crypto</Link>
+      <Link to="/currencies">Currencies</Link>
+      <Link to="/indices">Indices</Link>
+      <Link to="/markets">Markets</Link>
+      <Switch>
+        <Route path="/crypto">crypto</Route>
+        <Route path="/currencies">currencies</Route>
 
-    <Link to="/crypto">Crypto</Link>
-    <Link to="/currencies">Currencies</Link>
-    <Link to="/indices">Indices</Link>
-    <Link to="/markets">Markets</Link>
+        <Route path="/indices">
+          <IndicesPage />
+        </Route>
 
-    <Switch>
-      <Route path="/crypto">
-        crypto
-      </Route>
-      <Route path="/currencies">
-      currencies
-      </Route>
+        <Route path="/markets">markets</Route>
 
-      <Route path="/indices">
-    indices
-      </Route>
-
-      <Route path="/markets">
-markets
-      </Route>
-
-
-      <Route path="/">
-        
-      </Route>
-    </Switch>
+        <Route path="/"></Route>
+      </Switch>
     </div>
-  )
+  );
 }
 
 export default App;
