4bdfbc9d7c13e310ec07af995dac343db38ae9e0
Sebastian Orbom <zebban90a@gmail.com>
2020-12-11 09:26:52 +0100

    useEffect, map json

--- src/pages/CurrenciesPage.jsx
+++ src/pages/CurrenciesPage.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import CurrencieItem from "../components/CurrencieItem";
 
-export default function CurrenciesPage() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+export const CurrenciesPage = () => {
+  const [currenciesList, setCurrenciesList] = useState(null);
+
+  useEffect(() => {
+    const url =
+      "https://market-data-collector.firebaseio.com/market-collector/currencies.json";
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setCurrenciesList(data));
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row">
+        {indicesList &&
+          Object.entries(currenciesList).map(CurrencieItem => {
+            const key = CurrencieItem[0];
+            const value = CurrencieItem[1];
+            return <CurrencieItem key={key} index={value} />;
+          })}
+      </div>
+    </div>
+  );
+};
