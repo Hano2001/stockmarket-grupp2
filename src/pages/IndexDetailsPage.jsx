e4bc327bc410af6e1a7cc636b9b18196f1288cff
Oz90 <gurr__@hotmail.com>
2020-12-14 15:45:42 +0100

    Add color based on change to the indexdetailspage

--- src/pages/IndexDetailsPage.jsx
+++ src/pages/IndexDetailsPage.jsx
@@ -31,10 +31,38 @@ export const IndexDetailsPage = props => {
                 {indexInfo.today}
               </span>
             </div>
-            <div className="col-md-2">Change 1W {indexInfo.w1}%</div>
-            <div className="col-md-2">Change 3Y {indexInfo.y3}%</div>
-            <div className="col-md-2">Change 5Y {indexInfo.y5}%</div>
-            <div className="col-md-2">Change YTD {indexInfo.ytd}%</div>
+            <div className="col-md-2">
+              Change 1W &nbsp;
+              <span
+                className={indexInfo.w1 > 0 ? "text-primary" : "text-danger"}
+              >
+                {indexInfo.w1}
+              </span>
+            </div>
+            <div className="col-md-2">
+              Change 3Y &nbsp;
+              <span
+                className={indexInfo.y3 > 0 ? "text-primary" : "text-danger"}
+              >
+                {indexInfo.y3}
+              </span>
+            </div>
+            <div className="col-md-2">
+              Change 5Y &nbsp;
+              <span
+                className={indexInfo.y5 > 0 ? "text-primary" : "text-danger"}
+              >
+                {indexInfo.y5}
+              </span>
+            </div>
+            <div className="col-md-2">
+              Change YTD &nbsp;
+              <span
+                className={indexInfo.ytd > 0 ? "text-primary" : "text-danger"}
+              >
+                {indexInfo.ytd}
+              </span>
+            </div>
           </>
         )}
       </div>
