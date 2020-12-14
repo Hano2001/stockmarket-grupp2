ea2cb11fdc7ed54db632ea9a6a3a9c29db1fe87e
Oz90 <gurr__@hotmail.com>
2020-12-14 12:38:45 +0100

    Add a navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="nav mb-4 border-bottom border-primary justify-content-end">
+      <Link className="nav-link" to="/crypto">
+        Crypto
+      </Link>
+      <Link className="nav-link" to="/currencies">
+        Currencies
+      </Link>
+      <Link className="nav-link" to="/indexes">
+        Indices
+      </Link>
+      <Link className="nav-link" to="/markets">
+        Markets
+      </Link>
+    </nav>
+  );
+}
