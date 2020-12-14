ac42764a0dd4857ff1f6646d203bb4d7b8ae7d3b
Harald <Harald.Norlin@yh.nackademin.se>
2020-12-14 11:52:39 +0100

    Fix listpage

--- src/components/CryptoItem.jsx
+++ src/components/CryptoItem.jsx
@@ -3,11 +3,22 @@ import {Link} from 'react-router-dom'
 export default function CryptoItem({item}) {
     
     return (
-        <div className="mb-4 col-md-4 shadow p-3">
-        <p>NAME: {item.name}</p>
-        <p>LINK: {item.link}</p>
-        <p>TODAY: {item.today}</p>
+        <div className="container">
+        <div className=" row shadow-sm p-3 mb-5 bg-white rounded">
+        <h1>{item.name}</h1>
+        <p>({item.ticker})</p>
+        <div className="col-md-4 ml-3">
+          <div className="row">
+            <h4>Current price: &nbsp;</h4>
+            <h4> €{item.price}</h4>
+          </div>
+          <div className="row">
+            <h4>Change 1D: &nbsp;</h4>
+            <h4>{item.today}%</h4>
+          </div>
+        </div>
         <Link  className="btn btn-block btn-danger" to={`/crypto/${item.ticker}`}>Detaljer</Link>
+        </div>
         </div>
     )
       
