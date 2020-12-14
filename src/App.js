393800b40369a331168873c2852c4178d8da20f4
Axel Thiel <axth2220@gmail.com>
2020-12-14 10:21:33 +0100

    bla

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { IndicesPage } from "./pages/IndicesPage";
 import { IndexDetailsPage } from "./pages/IndexDetailsPage";
+import { MarketsPage } from "./pages/MarketsPage";
+import {MarketPage} from "./pages/MarketPage"
 
 function App() {
   return (
@@ -19,8 +21,10 @@ function App() {
           <IndicesPage />
         </Route>
 
-        <Route path="/markets">markets</Route>
-
+        <Route path="/markets/:market" component={MarketPage} />
+        <Route path="/markets">
+            <MarketsPage />
+            </Route>    
         <Route path="/"></Route>
       </Switch>
     </div>
