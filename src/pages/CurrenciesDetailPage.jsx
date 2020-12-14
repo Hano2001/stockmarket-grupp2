8bf95329345710b3fc86f08e97d81010a2d7140f
Sebastian Orbom <zebban90a@gmail.com>
2020-12-14 13:29:24 +0100

    add props for currency item

--- src/pages/CurrenciesDetailPage.jsx
+++ src/pages/CurrenciesDetailPage.jsx
@@ -17,10 +17,35 @@ export default function CurrenciesDetailPage(props) {
     return (
         <div className="container">
             <div className="row">
-            {!currencyItem ? <p>Loading</p> : <h2> {currencyItem.name}</h2> }
-            
-                
-                
+            {!currencyItem ? <p>Loading</p> : 
+            <>
+             <div className= "col-md-12"> 
+            <h2> {currencyItem.name}</h2>
+            </div>
+            <div className = "col-md-2">
+            <p>Price: {currencyItem.price}%</p>
+            </div>
+            <div className = "col-md-2">
+            <p>Today: {currencyItem.today}%</p>
+            </div>
+            <div className = "col-md-2">
+            <p>1week: {currencyItem.w1}%</p>
+            </div>
+            <div className = "col-md-2"> 
+            <p>1mtd: {currencyItem.mtd}%</p>
+            </div>
+            <div className = "col-md-2"> 
+            <p>3y: {currencyItem.y3}%</p>
+            </div>
+            <div className = "col-md-2"> 
+            <p>5y: {currencyItem.y5}%</p>
+            </div>
+            <div className = "col-md-2"> 
+            <p>ytd: {currencyItem.ytd}%</p>
+            </div>
+
+            </>
+             }  
             </div>
         </div>
     )
