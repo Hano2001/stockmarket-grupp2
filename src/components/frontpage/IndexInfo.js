33d86f01a6a98920d97aae51315186ce026b5cdd
Axel Thiel <axth2220@gmail.com>
2020-12-15 13:59:50 +0100

    add splash page

--- src/components/frontpage/IndexInfo.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function IndexInfo() {
-  return <div></div>;
-}
--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
-import CryptoInfo from '../components/frontpage/CryptoInfo'
-import CurrencyInfo from '../components/frontpage/CurrencyInfo'
-import IndexInfo from '../components/frontpage/IndexInfo'
-import StockholmInfo from '../components/frontpage/StockholmInfo'
-
-//Hämta data från alla endpoints. Crypto, Currency, Index, Stockholmsbörsen, gör detta i varje enskild komponent?
-//En bootstrap komponent som man fyller med data? bootstrap-kort kanske? Skapa en array som sorteras. mappa 5 första i ena och 5 sistsa i andra
-//för varje lägg in en komponent som visar topp 5 vinnare och topp 5 förlorare för varje kategori.
+import { InfoContainer } from '../components/InfoContainer'
 
 
 export const FrontPage = () => {
     return (
-        <div>
-            <CryptoInfo />
-            <CurrencyInfo />
-            <IndexInfo />
-            <StockholmInfo />
-        </div>
+        <>
+            <InfoContainer url="https://market-data-collector.firebaseio.com/market-collector/crypto/usd.json" category='Crypto' />
+            <InfoContainer url="https://market-data-collector.firebaseio.com/market-collector/currencies/sek.json" category='Currency'/>
+            <InfoContainer url="https://market-data-collector.firebaseio.com/market-collector/indexes/se.json" category='Index'/>
+            <InfoContainer url="https://market-data-collector.firebaseio.com/market-collector/markets/stockholmsborsen.json" category="Stockholmsbörsen" />
+        </>
     )
 }
