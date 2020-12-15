8cc2ec97ce2039d52e651106e9dea9d468c05744
Antonio A R <antonio.axelsson@gmail.com>
2020-12-15 11:41:10 +0100

    remove "Name:" and correct typo from currencyitem

--- src/components/CurrencyItem.jsx
+++ src/components/CurrencyItem.jsx
@@ -5,7 +5,7 @@ export default function CurrencyItem({ currency }) {
   return (
     <div className="container">
       <div className="row shadow-sm p-3 mb-5 bg-white rounded">
-        <h1> Namn: {currency.name}</h1>
+        <h1>{currency.name}</h1>
         <p>({currency.ticker})</p>
         <div className="col-md-4 ml-3">
           <div className="row">
@@ -21,7 +21,7 @@ export default function CurrencyItem({ currency }) {
             className="btn btn-primary btn-block"
             to={`/currencies/${currency.ticker}`}
           >
-            Go to Currencie
+            Go to Currency
           </Link>
         </div>
       </div>
