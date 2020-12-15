cd727af821568758f1c61664eced4e745ff297e5
Harald <Harald.Norlin@yh.nackademin.se>
2020-12-15 15:34:47 +0100

    Minor changefix

--- src/components/IndexItem.jsx
+++ src/components/IndexItem.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 import checkChange from "../hooks/checkChange";
 
 export default function IndexItem({ index }) {
-  const x = checkChange(index);
+  const changeToday = checkChange(index.today);
   return (
     <li className="row shadow-sm mb-5 bg-white rounded">
       <Link className="col-md-5" to={`/indexes/${index.ticker}`}>
         <h4>{index.name}</h4>
       </Link>
       <h4 className="col-md-5">€{index.price}</h4>
-      <h4 className={`col-md-2 ${x}`}>{index.today}%</h4>
+      <h4 className={`col-md-2 ${changeToday}`}>{index.today}%</h4>
     </li>
   );
 }
--- src/hooks/checkChange.js
+++ src/hooks/checkChange.js
@@ -1,5 +1,5 @@
 export default function checkChange(value) {
-  const change = value.today > 0 ? "text-primary" : "text-danger";
+  const change = value > 0 ? "text-primary" : "text-danger";
 
   return change;
 }
