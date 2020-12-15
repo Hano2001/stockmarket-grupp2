d9227178ac22bae6eac9dcec07a8ad5d3a30a23b
Sebastian Orbom <zebban90a@gmail.com>
2020-12-15 16:44:51 +0100

    bootstrap styling

--- src/pages/CurrencyDetailPage.jsx
+++ src/pages/CurrencyDetailPage.jsx
@@ -14,60 +14,61 @@ export default function CurrencyDetailPage(props) {
       <Link to={`/currencies`}>
         <h5>Back to Currency List</h5>
       </Link>
+      <div className ="row">
       {!currencyItem ? (
         <p>Loading</p>
       ) : (
         <>
           <div className="col-md-12">
             <h2> {currencyItem.name}</h2>
+            <h5>Current trading at:  {currencyItem.price} SEK</h5>
           </div>
-          <div className="col-md-4">
-            <h5>Price SEK: {currencyItem.price}</h5>
-          </div>
-          <div className="row">
-            <div className="col-md-2">
+          
+            <div className="col-md-4 mb-2">
               Change 1D: &nbsp;
               <span className={checkChange(currencyItem.today)}>
                 {currencyItem.today}%
               </span>
             </div>
-            <div className="col-md-2">
+            <div className="col-md-4">
               Change 1W : &nbsp;
               <span className={checkChange(currencyItem.w1)}>
                 {currencyItem.w1}%
               </span>
             </div>
-            <div className="col-md-2">
+            <div className="col-md-4">
               Change 1M: &nbsp;
               <span className={checkChange(currencyItem.mtd)}>
                 {currencyItem.mtd}%
               </span>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-md-2">
+          
+          
+            <div className="col-md-4">
               Change 3y: &nbsp;
               <span className={checkChange(currencyItem.y3)}>
                 {currencyItem.y3}%
               </span>
             </div>
 
-            <div className="col-md-2">
+            <div className="col-md-4">
               Change 5y: &nbsp;
               <span className={checkChange(currencyItem.y5)}>
                 {currencyItem.y5}%
               </span>
             </div>
 
-            <div className="col-md-2">
+            <div className="col-md-4">
               Change Ytd: &nbsp;
               <span className={checkChange(currencyItem.ytd)}>
                 {currencyItem.ytd}%
               </span>
             </div>
-          </div>
+          
+          
         </>
       )}
+      </div>
     </div>
   );
 }
